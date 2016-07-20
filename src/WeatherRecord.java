ceb24156072d6e027b5807badf967e28772aa018
proxna <pawel.gajzler.pg@gmail.com>
2016-07-20 20:02:53 +0200

    first commit

---
+++ src/WeatherRecord.java
@@ -0,0 +1,11 @@
+/**
+ * Created by GPDellKonto on 2016-05-14.
+ */
+public class WeatherRecord {
+    public double temperature;
+    public String weather;
+    public double windspeed;
+    public String city;
+    public String date;
+    public int hour;
+}
